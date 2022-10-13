2b8382d5376ec5ea5bb866350d2dea4abfda08a3
bangng <montersb@gmail.com>
2022-10-13 16:01:05 +0700

    b1

---
+++ src/main/java/rikkei/academy/controller/CalculateController.java
@@ -0,0 +1,19 @@
+package rikkei.academy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CalculateController {
+    @PostMapping("/calculate")
+    public String calculate(@RequestParam float usd, @RequestParam float rate, Model model) {
+
+        float result = usd * rate;
+
+        model.addAttribute("result", result);
+
+        return "result";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/result.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 10/13/2022
+  Time: 3:59 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<a href="/">Back</a>
+
+Result: ${result}
+</body>
+</html>
